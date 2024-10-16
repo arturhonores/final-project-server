c58f4f3fb3e166975c9c3ab74bd9c1eaf0d71c7f
arturhonores <luis.arturhonores@gmail.com>
2024-10-16 22:41:47 +0200

    actualizacion 16-10-24

--- db/index.js
+++ db/index.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/server";
 
+// Agrega este console.log para verificar el valor de MONGO_URI
+console.log("MongoDB URI: ", MONGO_URI);
+
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
